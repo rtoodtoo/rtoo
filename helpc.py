e1ad8ae95b9090bef6dfcd8b5d360712f82ab72f
Genco Yilmaz <tirexxerit@gmail.com>
2011-08-23 15:55:26 -0400

    rtoo.py help changes

--- helpc.py
+++ helpc.py
@@ -3,29 +3,71 @@
 class helpc:
 
     def help_set(self):                                                                                   
-        print "sytax: set {arguments}"                                                                       
-        print """usage:                                                                                  
-        pool {name} {bandwidth}      
-        bandwidth {name} {network} {bandwidth} {pool_name} {exceed|no-exceed} [comment]
+        print "syntax: set {options}"                                                                       
+        print """USAGE:                                                                                  
+        set pool {pool_name} bandwidth {rate} [comment]
+        set pool {pool_name} status    [remove|inactive|active]      
+        set bandwidth {member} {network} {bandwidth} {pool_name} [exceed|no-exceed] [comment]
+        set member {member_name} [remove|inactive|active] 
         
-        pool: assigns bandwidth to a pool defined 
-        bandwidth:   sets bandwidth for a specific network and ties it into shared pool
-                     >name: string to identify this specific network only alphanumeric characters allowed
-                     >network: Network address in notation 192.168.1.0/24
-                     >bandwidth: the rate in kbit or Mbit e.g 128kbit or 2Mbit
-                     >pool_name: the pool that this network range is bound to 
-                     >exceed: If set, user can exceed its link speed upto the pool it is bound to
-                     >no-exceed: If set, user cannot exceed its link speed defined
-                     >comment: optional to keep some information for the admin
+        [HELP]
+        >pool: creates a new pool
+          pool_name: the new of the pool
+            >bandwidth: To set a bandwidth limit on specified rate
+                 rate: the rate in kbit or Mbit e.g 128kbit or 2Mbit
+            >status: Modifies status of pool            
+                 *remove: removes the pool from bandwidth management system along with ALL connected members!!!
+                 *inactive: temporarily make this pool inactive by which ALL members using this pool will have no traffic shaping
+                 *active: activates an inactive pool which activates shaping in all connected members 
+        
+        >bandwidth: sets bandwidth for a specific network in units kbit or Mbit and binds it into a shared pool
+                        *member: name to identify this specific network only alphanumeric characters allowed
+                        *network: Network address in notation 192.168.1.0/24
+                        *bandwidth: the rate in kbit or Mbit e.g 128kbit or 2Mbit
+                        *pool_name: the pool that this network range is bound to 
+                        *exceed: If set, user can exceed its link speed upto the pool it is bound to
+                        *no-exceed: If set, user cannot exceed its link speed defined
+                        *comment: optional argument to keep some information for the admin
+
+         >member: performs operation on specified member
+                *member_name: the member which should be changed
+                    *remove: removes the member from bandwidth management system!!!
+                    *inactive: temporarily makes traffic shaping inactive 
+                    *active: enables shaping back  
 
 
-        Example commands:
+        [EXAMPLE COMMANDS]
 
         set pool Engineering 2Mbit
         set pool Students 512Kbit
         set bandwidth John 192.168.10/32 128kbit Engineering no-exceed
-        set bandwidth Bob 192.168.2.20/32 64kbit Students exceed
+        set bandwidth Bob 192.168.2.20/32 64kbit Students exceeda
+        set pool Engineering inactive
+        set member John inactive
         """                                                                                                
+    def help_show(self):
+        print "syntax: show {options}"
+        print """USAGE:
+        show traffic {pool|member} [pool_name|member_name]
+        show conf 
+
+        [HELP]
+        traffic: display active flowing traffic for the specified entity
+                 >pool: to show traffic in pool
+                     *pool_name: name of the pool 
+                 >member: to show traffic for a specific network
+                     *member_name: name of a registered member in rtoo
+        
+        conf : shows the active configuration made so far 
+        
+        [EXAMPLE COMMANDS]:
+        show traffic name John
+        show traffic name all
+        show traffic pool Engineering
+        """
+                 
+        
+          
 
     def help_error(self):
         print "Please check your input as it contains errors"
--- rtoo.py
+++ rtoo.py
@@ -8,7 +8,6 @@ import helpc
 import dbconn
 
 RtooVersion="0.1b"
-
 m = dbconn.mycon()
 conn = m.conn
 
@@ -19,11 +18,17 @@ class CLI(cmd.Cmd,rtooc.IPT,rtooc.TC,helpc.helpc):
         print "Initializing Rtoo",RtooVersion,"\n",
         print "For command reference, type help\n"
         self.prompt = 'rtoo> '
-
+    
     def emptyline(self):
       pass
-
- 
+  
+     
+    def error_msg(self,error):
+      """To stop processing and printing error"""  
+      self.error = error
+      print ""
+      print "ERROR: "+self.error
+      print ""
 
     def do_set(self,command_args):
       
@@ -42,22 +47,42 @@ class CLI(cmd.Cmd,rtooc.IPT,rtooc.TC,helpc.helpc):
         #Check if arguments are provided,if not display help 
         try:
           self.pool_name = arg[1] 
-          self.bandwidth = arg[2]
+          self.pool_option = arg[2]
+          self.pool_option_value = arg[3]
         except IndexError:
           return self.help_set()
+       
+        #At this stage we can check if the pool name exists already
+        cursor = conn.cursor()
+        cursor.execute("SELECT pool_name from pool where pool_name=%s LIMIT 1",(self.pool_name))
+        if cursor.rowcount !=0:
+          return self.error_msg("Pool name is already defined")
+ 
+        #Comment is optional,be sure it doesn't cause problem
+        try:
+          self.comment = arg[4]
+        except IndexError:
+          self.comment = None 
 
-        #Set pool and add it into conf.ini file  
-        if self.SetTrafficPool(self.bandwidth) == 0:
-          #TC returns 0 only if it is successful
-          print "Pool : %s" % (self.pool_name)
-          print "Bandwidth: %s" % (self.bandwidth) 
-          print "Added successfully"
-          cursor = conn.cursor()
-          cursor.execute ("INSERT INTO pool (pool_id,pool_name,bandwidth,comment) VALUES (null,%s,%s,'no_comment')",(self.pool_name,self.bandwidth))
-          cursor.close()
-        else:
-          print "An error occured during pool allocation"
+        #Option to pool argument is bandwidth
+        if self.pool_option == 'bandwidth':
+           #We expect the rate in kbits and Mbits in here
+           self.rate =  self.pool_option_value
+           if self.SetTrafficPool(self.rate) == 0:
+             #TC returns 0 only if it is successful
+             print "Pool : %s" % (self.pool_name)
+             print "Bandwidth: %s" % (self.rate) 
+             print "Added successfully"
+             if not self.comment: 
+               self.comment = ""
+             cursor = conn.cursor()
+             cursor.execute ("INSERT INTO pool (pool_id,pool_name,bandwidth,comment) VALUES (null,%s,%s,%s)",(self.pool_name,self.rate,self.comment))
+             cursor.close()
+           else:
+             print "An error occured during pool allocation"
       
+     
+ 
       #Firt argument is address, it is a name for network address
       #and must be in conf.ini file
       if arg[0] == "bandwidth":
@@ -74,13 +99,13 @@ class CLI(cmd.Cmd,rtooc.IPT,rtooc.TC,helpc.helpc):
        ##Checks: bandwidth,network,address_name for proper format
        ##       
        
-      if re.search(r'\W+',self.address_name):
-         print "Address name cannot have non-alpanumeric characters"
-         return self.help_shell() 
-      if not re.search(r'^\d+(kbit|Mbit)$',self.bandwidth):
-         print self.bandwidth
-         print "Bandwidth format is incorrect"
-         return self.help_shell()
+        if re.search(r'\W+',self.address_name):
+           print "Address name cannot have non-alpanumeric characters"
+           return self.help_shell() 
+        if not re.search(r'^\d+(kbit|Mbit)$',self.bandwidth):
+           print self.bandwidth
+           print "Bandwidth format is incorrect"
+           return self.help_shell()
 
        #cursor = conn.cursor()
        #cursor.execute ("INSERT INTO bandwidth (address_id,address_name,network,bandwidth,exceed,comment) VALUES (null,%s,%s,'no_comment')",(self.pool_name,self.bandwidth))
